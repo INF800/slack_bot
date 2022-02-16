800dc70d18f3e48b5394c81ff0af4c90daa6060f
Asapanna Rakesh <45640029+INF800@users.noreply.github.com>
2022-02-16 04:59:19 +0000

    done

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+# how to generate URL https://www.youtube.com/watch?v=lEQ68HhpO4g 
+INCOMING_WEBHOOKS_ACCESS_URL=os.getenv("INCOMING_WEBHOOKS_ACCESS_URL")
+
+
+def send_message(post_data, api_url, headers={'Content-Type': 'application/json'}):
+    response = requests.post(api_url, headers=headers, json=post_data) 
+    print(response.status_code, response.content)
+
+
+def generate_post_data(markdown_texts):
+    # https://api.slack.com/messaging/composing/layouts#attachments
+    if type(markdown_texts)!=list:
+        markdown_texts = [markdown_texts]
+    post_data = {'blocks': []}
+    for text in markdown_texts:
+        content = {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": text
+            }
+        }
+        post_data['blocks'].append(content)
+    print(post_data)
+    return post_data
+
+def main():
+    post_data = generate_post_data("```hellow!!```")
+    send_message(post_data, api_url=INCOMING_WEBHOOKS_ACCESS_URL)
+
+if __name__=='__main__':
+    main()
